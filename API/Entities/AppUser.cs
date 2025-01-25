d24d46e34937fd1223387f3d86c9443ae1a03bc5
blyssco <youssefboulaich@outlook.com>
2025-01-25 14:03:26 +0100

    25012025

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,4 +5,6 @@ public class AppUser
 {
    public int Id { get; set; }
    public required string UserName { get; set; }
+   public required byte[] passwordHash { get; set; }
+   public required byte[] passwordSalt { get; set; }
 }
